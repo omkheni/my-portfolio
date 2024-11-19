61f1150f2448cae8ca4d0ee1b640c5b1adc0b906
Om Kheni <kheniom29@gmail.com>
2024-11-19 21:45:11 +1030

    header implementation

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">Om Kheni</div>
+      <nav>
+        <ul>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
